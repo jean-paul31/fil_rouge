2bdbc27f1b9a81c3beae3dca471ca22298c3a229
Hamelet <58843994+Hamelet@users.noreply.github.com>
2020-07-27 07:06:46 +0200

    try to fix

--- mediaschool/src/app/services/posts.service.ts
+++ mediaschool/src/app/services/posts.service.ts
@@ -12,6 +12,7 @@ import * as firebase from 'firebase';
 export class PostsService {
 
   @Output() posts: Post[] = [];
+  idPost = 0;
 
   postsSubject = new Subject<Post[]>();
 
@@ -26,11 +27,12 @@ export class PostsService {
   }
 
   savePosts() {
-    firebase.database().ref('/blog').set(this.posts);
+    firebase.database().ref(`/blog/${this.idPost}`).set(this.posts);
+    this.idPost += 1;
   }
 
   getPosts() {
-    firebase.database().ref('/blog')
+    firebase.database().ref(`/blog/${this.idPost}`)
       .on('value', (data: DataSnapshot) => {
         this.posts = data.val() ? data.val() : [];
 
